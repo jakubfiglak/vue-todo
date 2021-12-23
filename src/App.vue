eb23f6789141800c209cd0e5e594d8f0d39d4cd1
Jakub Figlak <jakub.figlak@netguru.pl>
2021-12-23 15:03:58 +0100

    Implement basic theme switch

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Hello</h1>
+    <button @click="toggleTheme">Toggle Theme</button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+type Theme = 'light-theme' | 'dark-theme';
+type Data = {
+  userTheme: Theme;
+};
 
 export default Vue.extend({
   name: 'App',
-  components: {
-    HelloWorld,
+  mounted() {
+    const initUserTheme = this.getMediaPreference();
+    this.setTheme(initUserTheme);
+  },
+  data(): Data {
+    return {
+      userTheme: 'light-theme',
+    };
+  },
+  methods: {
+    setTheme(theme: Theme) {
+      localStorage.setItem('user-theme', theme);
+      this.userTheme = theme;
+      document.documentElement.className = theme;
+    },
+    toggleTheme() {
+      const activeTheme = localStorage.getItem('user-theme') as Theme | undefined;
+      if (activeTheme === 'light-theme') {
+        this.setTheme('dark-theme');
+      } else {
+        this.setTheme('light-theme');
+      }
+    },
+    getMediaPreference(): Theme {
+      const hasDarkPreference = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+      if (hasDarkPreference) {
+        return 'dark-theme';
+      }
+      return 'light-theme';
+    },
   },
 });
 </script>
@@ -25,5 +58,14 @@ export default Vue.extend({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: var(--background-color);
+}
+
+:root {
+  --background-color: white;
+}
+
+:root.dark-theme {
+  --background-color: black;
 }
 </style>
